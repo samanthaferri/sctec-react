e45b02a61e2ab4cad996a9e9376831fe12f4f3f8
Samantha Manuela Ferri Tavares <samanthamanuelaferri@gmail.com>
2026-09-24 23:20:58 -0300

    exercicios

--- modulo-01/aula-04/exercicios.js
+++ modulo-01/aula-04/exercicios.js
@@ -1,4 +1,4 @@
-//conte de 5 até 1 e escreva Já
+/* //conte de 5 até 1 e escreva Já
 for (i = 5; i >= 1; i--) {
     console.log(i);
 }
@@ -12,12 +12,21 @@ while (cont <= 10) {
     soma = soma + cont;
     cont++;
 }
-
+ */
 //gere um número (de 1 a 5) para o usuário adivinhar até acertar
 const prompt = require('prompt-sync')();
 let numero = 3;
 let adivinhacao;
+
 do {
-    adivinhacao = prompt("Escolha um número de 1 a 5 (usando somente números): ");
-    Number.isNaN(adivinhacao);
-} while(numero);
+    adivinhacao = Number(prompt("Escolha um número de 1 a 5 (usando somente números): "));
+
+    if (Number.isNaN(adivinhacao)) {
+        console.log("sam");
+    }
+    if (adivinhacao === numero) {
+        console.log("Você acertou!");
+    } else {
+        console.log("Você errou, tente novamente.");
+    }
+} while (adivinhacao !== numero);
---
+++ modulo-01/aula-05/exercicios.js
@@ -0,0 +1,24 @@
+/* Criar um programa que calcula o preço final de um produto com desconto
+- criar variáveis para preço original e percentual de desconto
+- criar uma função para calcular o desconto que retorna o valor final do produto
+- usar if-else para aplicar o desconto apenas para compras acima de 100 reais */
+
+const prompt = require('prompt-sync')();
+
+function precoFinal(preco, desconto) {
+    return Number(preco) - Number(preco * desconto / 100);
+}
+
+let precoOriginal = Number(prompt("Qual é o valor do produto (ex.: 100)? R$"));
+while (Number.isNaN(precoOriginal)){
+    precoOriginal = Number(prompt("Valor inválido! Insira novamente o valor do produto: R$:"))
+}
+
+let descontoOferecido;
+if(precoOriginal > 100) {
+    descontoOferecido = Number(prompt("Qual é a porcentagem do desconto (ex.: 10)? "));
+    console.log(`O valor original do produto é R$${precoOriginal} e o desconto é de ${descontoOferecido}%`);
+    console.log(`Então, o preço final: ${precoFinal(precoOriginal, descontoOferecido).toFixed(2)}`)
+}
+
+
